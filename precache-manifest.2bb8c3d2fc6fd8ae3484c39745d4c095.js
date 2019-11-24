5d6c989d4b30cd2a5a76d7ca40209a30c9d8e94a
Mikhail Karpov <mihanikk@gmail.com>
2019-11-24 15:28:53 +0300

    Updates

---
+++ precache-manifest.2bb8c3d2fc6fd8ae3484c39745d4c095.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c937de969a6b3a16c2e",
+    "url": "/scrolling-message-app/static/js/runtime~main.0c937de9.js"
+  },
+  {
+    "revision": "42e6fdfaf4f2fbfa2da6",
+    "url": "/scrolling-message-app/static/js/main.42e6fdfa.chunk.js"
+  },
+  {
+    "revision": "26a8c34e341d70a584e2",
+    "url": "/scrolling-message-app/static/js/1.26a8c34e.chunk.js"
+  },
+  {
+    "revision": "42e6fdfaf4f2fbfa2da6",
+    "url": "/scrolling-message-app/static/css/main.61a3a1d1.chunk.css"
+  },
+  {
+    "revision": "e5b0ef120a8e88b94ec02a98ab7e7b73",
+    "url": "/scrolling-message-app/index.html"
+  }
+];
